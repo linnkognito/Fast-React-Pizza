42b585894dafd739913f7e330727cbb278ce10c5
Linn Johansen <linn.es.johansen@gmail.com>
2025-01-14 14:49:14 -0800

    Initial setup: Routes, Statis components

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order',
+        element: <Order />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderID',
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Pizza Co.</Link>
+
+      <p>Placeholder header paragraph.</p>
+    </header>
+  );
+}
+
+export default Header;
